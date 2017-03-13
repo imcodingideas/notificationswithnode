dc93a8d63ac61902c1bf0bbbbf50b9f4cf80c464
Joseph Chambers <imcodingideas@Josephs-MacBook-Pro.local>
2017-03-13 12:15:33 -0700

    I can get feedback from notifications!!!

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-const notifier = require('node-notifier');
+const NotificationCenter = require('node-notifier').NotificationCenter;
 
-notifier.notify('Go buy dog food!');
+let notifier = new NotificationCenter({
+    withFallBack: true,
+    customPath: void 0
+})
 
 notifier.notify({
-  'title': 'Pay The Bills',
-  'subtitle': 'Monthly Maintenance',
-  'message': 'Go pay your bills',
-  'wait': true
+  'title': void 0,
+  'subtitle': void 0,
+  'message': "Click 'reply' to send a message back",
+  'icon': 'Terminal Icon',
+  'contentImage': void 0,
+  'open': void 0,
+  'wait': false,
+
+  timeout: 10,
+  closeLabel: void 0, 
+  actions: void 0,
+  dropdownLabel: void 0,
+  reply: false
+}, (error, response, metadata) => {
+    console.log(error, response, metadata);
 });
